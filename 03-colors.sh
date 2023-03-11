0b0845e270a0d0e00be2e77c6dfee1a17fdec215
Muneer <muneersurya@gmail.com>
2023-03-11 21:32:36 +0530

    Auto Push

--- 03-colors.sh
+++ 03-colors.sh
@@ -22,3 +22,13 @@
  echo -e "\e[35m i am printing magenta color \e[0m"
  echo -e "\e[36m i am printing cyan color \e[0m"
 
+
+# How to print the background color
+
+#      echo -e "\e[BackgroundCOL-CODE;ForegroundColorm  Your mesage to be printed \e[0m"
+ echo -e "\e[43;32m i am printing green color with YELLOW as background \e[0m"
+ echo -e "\e[44;31m i am printing red color with BLUE as background \e[0m"
+ echo -e "\e[45;33m i am printing yellow color with MAGENTA as background \e[0m"
+ echo -e "\e[46;34m i am printing blue color with CYAN as background \e[0m"
+ echo -e "\e[47;35m i am printing magenta color with WHITE as background \e[0m"
+ echo -e "\e[42;36m i am printing cyan color with GREEN as background \e[0m"
