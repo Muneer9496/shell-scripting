b8a3257bb1ecbb3936a4c7dc7fb8352fbcbb8e05
Muneer <muneersurya@gmail.com>
2023-03-11 22:54:34 +0530

    Auto Push

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -3,4 +3,4 @@
 TODAYDATE=$(date +%F) # this is way od declaring is called hardway of coding
 
 echo -e "Welcome to bash training, todays date is \e[32m ${TODAYDATE} \e[0m"
-echo "number of users logged into the system is : $(who | ec -l)"
+echo "number of users logged into the system is : $(who | wc -l)"
