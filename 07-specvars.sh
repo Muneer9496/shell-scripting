53a3ac3daf724374411f1ee34aeeba5325ec5cc1
Muneer <muneersurya@gmail.com>
2023-03-11 23:49:10 +0530

    Auto Push

--- 07-specvars.sh
+++ 07-specvars.sh
@@ -5,4 +5,8 @@ echo $1   # takes the first value from the command line
 echo $2   # takes the second value from the command line
 
 echo $*   # $* is going to print the used variables
-echo $@
+echo $@   # $@ is going to print the used variables
+echo $$   # $$ is going to print the PID of the current process
+echo $#   # $# is going to print the number of arguments
+
+# $* or $@ both of them prints used variables in the script
