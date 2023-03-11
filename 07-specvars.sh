5c435945f13640ae1edacb131386b07e7adfe4c5
Muneer <muneersurya@gmail.com>
2023-03-11 23:39:32 +0530

    Auto Push

---
+++ 07-specvars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo $0   # prints script name
+echo $1   # takes the first value from the command line
+echo $2   # takes the second value from the command line
+
+echo $*   # $* is going to print the used variables
+echo $@
