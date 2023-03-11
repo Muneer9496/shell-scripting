da8d4e108a31e8af9c43dd023c5a303f6b1ffbd0
Muneer <muneersurya@gmail.com>
2023-03-11 23:05:19 +0530

    Auto Push

--- 06-specialvariables.sh
+++ 06-specialvariables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # special characters : $0 to $9, $*, $#, #@, $& 
-echo $0 # gives the names of the script you are running
+echo "Name of the script is : $0   # gives the names of the script you are running
