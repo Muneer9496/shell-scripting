407774dd68b9d362679edb493ac7f51acfd2ed76
Muneer <muneersurya@gmail.com>
2023-03-11 16:02:37 +0530

    Auto Push

---
+++ 01-shebang.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# start of the line in any bash script should be a SHEBANG  notation.
+
+# apart from the line, none of the lines are considered as shebang
+# And shebang notation should always start with #
+# SHEBANG : using this notation, we are going to tell the system that the interpretor to be used for this script is bash, python or shell.
+# Apart from the line 1 or first of the script, anything which starts with '#' will be referred as a "COMMENT"
+# what is COMMENT? comment is to add description or exclude something
+
+echo hello world
