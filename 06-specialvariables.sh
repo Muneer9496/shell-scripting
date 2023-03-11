daa7c28d3d1fe41507f372c214e0971fcb901c78
Muneer <muneersurya@gmail.com>
2023-03-11 23:09:30 +0530

    Auto Push

--- 06-specialvariables.sh
+++ 06-specialvariables.sh
@@ -2,3 +2,9 @@
 
 # special characters : $0 to $9, $*, $#, #@, $& 
 echo "Name of the script is : $0"   # gives the names of the script you are running
+
+echo first value is $1
+echo second value is $20
+echo third value is $566
+
+# bash scriptname.sh 100 200 300
