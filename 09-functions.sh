3df0478f29bdb41d55021e39d48c112570280792
Muneer <muneersurya@gmail.com>
2023-03-12 00:20:41 +0530

    Auto Push

---
+++ 09-functions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# there are 4 types of commands available :
+
+# 1) binary             (/bin,  /sbin)
+# 2) aliases            (aliases are shortcuts alias net= "netstat -tulpn")
+# 3) shell built-in commands  
+# 4) functions           # functions are nothing but a set of command that can be written in a sequenc and called n number of times
