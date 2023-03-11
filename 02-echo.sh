455bb552951cbfc8825fdb615f2a59bab957f679
Muneer <muneersurya@gmail.com>
2023-03-11 21:23:06 +0530

    Auto Push

--- 02-echo.sh
+++ 02-echo.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 echo line1
 echo line2
---
+++ 03-colors.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Color	Foreground Code	Background Code
+# Black	      30	              40
+# Red	      31	              41
+# Green	      32	              42
+# Yellow	  33	              43
+# Blue	      34	              44
+# Magenta	  35	              45
+# Cyan	      36	              46
+# White	      37	              47
+
+ # the syntax to print color is
+ # Ex:
+ #      echo -e "\e[COL-CODE  Your mesage to be printed \e[0m"
+ #      echo -e "\e[32m i am printing green color \e[0m"
+
+ echo -e "\e[32m i am printing green color \e[0m"
+
