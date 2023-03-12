80040562968b86047ec8cdf1e3880ec6adaa4bfc
Muneer <muneersurya@gmail.com>
2023-03-12 23:36:19 +0530

    Auto Push

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -1,3 +1,111 @@
 #!/bin/bash
 
-echo "I am mongodb"
+
+# set -e
+
+COMPONENT=mongo
+LOGFILE=/tmp/$COMPONENT.log
+
+# validating whether the executed user is a root user or not
+ID=$(id -u) 
+
+if [ "$ID" -ne 0 ] ; then
+    echo -e "\e[31m you should execute this step as root user or with a sudo as prefix \e[0m"
+    exit 1
+fi
+
+stat () {
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32m Success \e[0m"
+    else 
+        echo -e "\e[31m Failure \e[0m"
+        exit 2
+    fi
+}
+
+
+echo -n "configuring the $COMPONENT repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
+
+echo -n "installing the $COMPONENT"
+yum install -y mongodb-org   &>> $LOGFILE
+stat $?
+
+
+
+
+
+# 1. Install Mongo & Start Service.
+
+# ```bash
+# # yum install -y mongodb-org
+# # systemctl enable mongod
+# # systemctl start mongod
+
+# ```
+
+# 1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in the config file, so that MongoDB can be accessed by other services.
+
+# Config file:   `# vim /etc/mongod.conf`
+
+# ![mongodb-update.JPG](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/87c01042-0f64-4ac4-ae5a-ffaf62836290/mongodb-update.jpg)
+
+# - Then restart the service
+
+# ```bash
+# # systemctl restart mongod
+# ```
+
+  
+
+# - Every Database needs the schema to be loaded for the application to work.
+
+# ---
+
+#       `Download the schema and inject it.`
+
+# ```
+# # curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+
+# # cd /tmp
+# # unzip mongodb.zip
+# # cd mongodb-main
+# # mongo < catalogue.js
+# # mongo < users.js
+# ```
+
+# Symbol `<` will take the input from a file and give that input to the command.
+
+# - Now proceed with the next component `CATALOGUE`
+
+
+
+
+
+
+echo -n "installing nginx : "
+yum install nginx -y &>> $LOGFILE
+stat $?
+
+echo -n "downloading the $COMPONENT components :"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
+
+echo -n "performing a cleanup of old $COMPONENT :"
+cd /usr/share/nginx/html
+rm -rf * &>> $LOGFILE
+stat $?
+
+echo -n "copying the downloaded $COMPONENT content :"
+unzip /tmp/$COMPONENT.zip &>> /$LOGFILE
+mv $COMPONENT-main/* .
+mv static/* .
+rm -rf $COMPONENT-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
+
+echo -n "starting the service :"
+systemctl enable nginx &>> $LOGFILE
+systemctl start nginx &>> $LOGFILE
+stat $?
