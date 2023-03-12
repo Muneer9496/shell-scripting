8a9cf1dc1a5bc0dc7aed7bba92ec85302e5bf344
Muneer <muneersurya@gmail.com>
2023-03-12 22:44:17 +0530

    Auto Push

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-# valoidating whether the executed user is a root user or not
+# validating whether the executed user is a root user or not
 ID=$(id -u) 
 
 if [ "$ID" -ne 0 ] ; then
@@ -11,21 +11,21 @@ if [ "$ID" -ne 0 ] ; then
     exit 1
 fi
 
-yum install nginx -y
+yum install nginx -y &>> /tmp/frontend.log
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
+rm -rf * &>> /tmp/frontend.log
+unzip /tmp/frontend.zip &>> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx &>> /tmp/frontend.log
+systemctl start nginx &>> /tmp/frontend.log
 
 
 
