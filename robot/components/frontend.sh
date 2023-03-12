564426d0d3d8dd055e388ad034467337f64a42e0
Muneer <muneersurya@gmail.com>
2023-03-12 22:19:46 +0530

    Auto Push

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+set -e
+
 yum install nginx -y
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
@@ -16,3 +18,15 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 systemctl enable nginx
 systemctl start nginx
+
+
+
+
+
+1) few of steps are failed, but still my sccript is executed irrespective of failure
+2) installation failed, why? i have not validated that i have root privileges
+3) the info that id like to provide is success or failure
+
+
+
+
